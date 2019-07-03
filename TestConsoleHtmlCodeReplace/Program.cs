31e991c692adc177c0b3854bfc36f8975bdbb719
mohammedibrahim.lj@gmail.com <mohammed.l@focuscbe.local>
2019-07-03 01:49:59 -0400

    Signed-off-by: mohammedibrahim.lj@gmail.com <mohammed.l@focuscbe.local>

---
+++ TestConsoleHtmlCodeReplace/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleHtmlCodeReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string s=  HTMLCodeReplacer.ReplaceString.PutString("Highlighted by a stylish silver/black full nylon frame design for superior protective comfort,&nbsp;3M™ BX Reader Series Safety Glasses are available in clear 1.50, 2.0 &amp; 2.50 diopter lenses. They feature adjustable length temples, three lens angles for a custom fit and nose bridge for comfort. The high impact polycarbonate lens filters 99.9% UV rays. DX anti-fog hard coat lens guards against fogging and are adjustable to better visibility. These safety glasses meet both the ANSI Z87.1-2003 and CSA Z94.3-2002 standards. Packaged 10 pair/box; 100 pair/case. Silver/black full nylon frame design for superior protective comfortAvailable in clear 1.5, 2.0 &amp; 2.5 diopter lensesAdjustable soft tip temples feel good even after extended wearSoft universal-fit nose bridgeHigh impact polycarbonate lens blocks 99.9% UVAnti-fog hard coat lens guards against fogging and are adjustable to better visibilityThree lens angles for custom fitMeet ANSI Z87.1-2003 and CSA Z94.3-2002 standards");
+        }
+    }
+}
